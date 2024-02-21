55bb87830303e25d278673f06dbe486ec9c4f484
Santiago E <santiago93echevarria@gmail.com>
2024-02-21 16:53:54 -0300

    cleanup

--- push-layer.sh
+++
@@ -1,8 +0,0 @@
-layer_path=".layers/layers/requests-layer.zip"
-
-aws lambda publish-layer-version --layer-name my-layer \
-    --description "My layer" \
-    --license-info "MIT" \
-    --zip-file fileb://$layer_path \
-    --compatible-runtimes python3.10 python3.11 \
-    --compatible-architectures "arm64" "x86_64"
